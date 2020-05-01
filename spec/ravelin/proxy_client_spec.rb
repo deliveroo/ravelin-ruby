b8f857b5521c5f3ec0cdcb3c58d17a3d5e2aa70f
Phil Hack <phil.hack@deliveroo.co.uk>
2020-05-01 13:53:01 +0100

    PRC-101: extracted out common shared_contexts for the ravelin client specs

--- spec/ravelin/proxy_client_spec.rb
+++ spec/ravelin/proxy_client_spec.rb
@@ -18,28 +18,6 @@ describe Ravelin::ProxyClient do
 
   let(:client) { described_class.new(base_url: base_url, url_prefix: url_prefix, username: username, password: password) }
 
-  shared_context 'event setup and stubbing' do
-    let(:event_name) { 'foobar' }
-    let(:event_payload) { { id: 'ch-123' } }
-    let(:event) do
-      double('event', name: event_name, serializable_hash: event_payload)
-    end
-
-    before { allow(client).to receive(:post) }
-  end
-
-  shared_context 'tag setup and stubbing' do
-    let(:tag_name) { :tagname }
-    let(:tag_payload) { { "customerId" => '123', "tagNames" => ['foo', 'bar'] } }
-    let(:tag) do
-      double('tag', name: tag_name, serializable_hash: tag_payload)
-    end
-
-    before { allow(client).to receive(:post) }
-    before { allow(client).to receive(:delete) }
-    before { allow(client).to receive(:get) }
-  end
-
   describe '#send_event' do
     include_context 'event setup and stubbing'
 
