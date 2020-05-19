86911bb0d18df70e469f2f5f783fdc67ffcc5c2e
Phil Hack <phil.hack@deliveroo.co.uk>
2020-05-19 08:38:28 +0100

    PRC-107: Added a test to ensure that the gem correctly handles when a warning does not exist, since warnings are not always returned by the ravelin API.

--- spec/ravelin/response_spec.rb
+++ spec/ravelin/response_spec.rb
@@ -25,6 +25,12 @@ describe Ravelin::Response do
         expect(response.source).to eq('detective-fraud')
         expect(response.comment).to eq('Looks pretty sketchy')
       end
+
+      context 'when the response does not include warnings' do
+        it 'does not set the warnings' do
+          expect(response.warnings).to be_nil
+        end
+      end
     end
 
     context 'when a tag is sent' do
@@ -42,6 +48,48 @@ describe Ravelin::Response do
         expect(response.label).to eq('GENUINE')
         expect(response.tags).to eq('1234abc')
         expect(response.client_reviewed_status).to eq(-1)
+      end
+    end
+
+    context 'when Ravelin returns warnings' do
+      let(:faraday_response) do
+        double('faraday', status: 200, body: {
+          'data' => {
+            'warnings'    => [
+              {
+                'class' => 'customer-absent-email',
+                'help' => 'https://developer.ravelin.com/apis/warnings/#customer-absent-email',
+                'msg' => "Customer \"eg-cust-a1\" does not have an email address."
+              },
+              {
+                'class' => 'customer-absent-reg-time',
+                'help' => 'https://developer.ravelin.com/apis/warnings/#customer-absent-reg-time',
+                'msg' => "Customer \"eg-cust-a1\" does not have a registration time."
+              },
+              {
+                'class' => 'order-absent-price',
+                'help' => 'https://developer.ravelin.com/apis/warnings/#order-absent-price',
+                'msg' => "Order \"25857597\" does not have a price."
+              }
+            ]
+          }
+        })
+      end
+
+      it('returns the correct number of warnings') do
+        expect(response.warnings.count).to eq(3)
+      end
+
+      it('returns the correct details for the first warning') do
+        expect(response.warnings.first['class']).to eq('customer-absent-email')
+        expect(response.warnings.first['help']).to eq('https://developer.ravelin.com/apis/warnings/#customer-absent-email')
+        expect(response.warnings.first['msg']).to eq("Customer \"eg-cust-a1\" does not have an email address.")
+      end
+
+      it('returns the correct details for the last warning') do
+        expect(response.warnings.last['class']).to eq('order-absent-price')
+        expect(response.warnings.last['help']).to eq('https://developer.ravelin.com/apis/warnings/#order-absent-price')
+        expect(response.warnings.last['msg']).to eq("Order \"25857597\" does not have a price.")
       end
     end
 
