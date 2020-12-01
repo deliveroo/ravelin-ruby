0d2fb101de5cb6b254f8b909b307c1f53fb0f92d
Brian Yarr <brian.yarr@deliveroo.co.uk>
2020-12-01 14:53:09 +0000

    Extract validation methods

--- lib/ravelin/app.rb
+++ lib/ravelin/app.rb
@@ -2,13 +2,25 @@ module Ravelin
   class App < RavelinObject
     attr_accessor :name, :platform, :domain
 
-    PLATFORM_VALUES = %w(ios android web mobile-web)
+    PLATFORM_IOS = 'ios'
+    PLATFORM_ANDROID = 'android'
+    PLATFORM_WEB = 'web'
+    PLATFORM_MOBILE_WEB = 'mobile-web'
+    PLATFORM_VALUES = [PLATFORM_IOS, PLATFORM_ANDROID, PLATFORM_WEB, PLATFORM_MOBILE_WEB]
 
     def validate
       super
 
-      raise ArgumentError, "Platform value be one of #{PLATFORM_VALUES.join(', ')}" unless platform.nil? || PLATFORM_VALUES.include?(platform)
-      raise ArgumentError, 'Domain does not match regex  ^[a-z0-9-\\.]+$' unless domain.nil? ||  /^[a-z0-9-\\.]+$/.match(domain)
+      raise ArgumentError, "Platform value be one of #{PLATFORM_VALUES.join(', ')}" unless App.valid_platform?(platform)
+      raise ArgumentError, 'Domain is not valid' unless App.valid_domain(domain)
+    end
+
+    def self.valid_platform?(platform)
+      platform.nil? || PLATFORM_VALUES.include?(platform)
+    end
+
+    def self.valid_domain(domain)
+      domain.nil? || /^[a-z0-9-\\.]+$/.match(domain)
     end
 
   end
